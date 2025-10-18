d2bce5fa37dc75aefc91d4dfc8e2fd7fb6b85166
merxgrc <mgarcia73@mail.sfsu.edu>
2025-10-17 21:18:18 -0700

    Test message

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 
-console.log("Hello world")
+const { Command } = require('commander');
+const simpleGit = require('simple-git');
+
+const program = new Command();
+const git = simpleGit();
+
+program
+  .name("git-dot")
+  .description("AI commit assistant")
+  .action(main)
+
+program.parse();
+
+async function main() {
+    try {
+        const result = await git.commit("Test message")
+        console.log(result.summary)
+    } catch(err) {
+        console.error("commit failed:", err.message);
+    }
+    
+}
 
